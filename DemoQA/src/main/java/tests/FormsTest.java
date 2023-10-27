22e3e3bade0bcea9851a053eab79c08286c17f82
phamduccong1234 <phamduccong1234@gmail.com>
2023-10-27 10:47:52 +0700

    push source code day 12

--- DemoQA/src/main/java/tests/FormsTest.java
+++ DemoQA/src/main/java/tests/FormsTest.java
@@ -3,42 +3,29 @@ package tests;
 import java.time.Duration;
 
 import org.testng.Assert;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import pages.FormsPage;
 import pages.HomePage;
 import pages.PracticeFormPage;
 import pages.PracticeFormResult;
+import tests.model.FormData;
 
 public class FormsTest extends TestCase {
 	/**
 	 * TextBox - TC01
 	 */
-	@Test
-	public void doTC01() {
-		
-		// Click vào phần tử
+	@Test(description = "TC01 - Submit data successfully", dataProvider = "FormData")
+	public void submitDataSuccessfully(String inputFirstName, String inputLastName, String inputEmail,
+			String inputGender, String inputDay, String inputMonth, String inputYear, String inputUserNumber,
+			String inputSubject1, String inputSubject2, String inputCurrentAddress, String inputCheckbox1,
+			String inputCheckbox2, String inputState, String inputCity) {
 		HomePage homePage = new HomePage(testBasic.driver);
 
 		FormsPage formsPage = homePage.clickForms();
 		PracticeFormPage practiceFormPage = formsPage.clickPracticeFormMenu();
-		
-		String inputFirstName = "Pham";
-		String inputLastName = "Cong";
-		String inputEmail = "abcd@gmail.com";
-		String inputGender = "Male";
-		String inputDay = "9";
-		String inputMonth = "December";
-		String inputYear = "1998";
-		String inputUserNumber = "0355373838";
-		String inputSubject1 = "Maths";
-		String inputSubject2 = "English";
-		String inputCurrentAddress = "Ha Noi";
-		String inputCheckbox1 = "Sports";
-		String inputCheckbox2 = "Reading";
-		String inputState = "NCR";
-		String inputCity = "Delhi";
-		
+
 		practiceFormPage.testBasic.inputText(practiceFormPage.txtFirstName, inputFirstName);
 		practiceFormPage.testBasic.inputText(practiceFormPage.txtLastName, inputLastName);
 		practiceFormPage.testBasic.inputText(practiceFormPage.txtUserMail, inputEmail);
@@ -53,21 +40,21 @@ public class FormsTest extends TestCase {
 		practiceFormPage.clickCheckboxByLabel(inputCheckbox2);
 		practiceFormPage.inputUploadPicture();
 		practiceFormPage.testBasic.inputText(practiceFormPage.txtCurrentAddress, inputCurrentAddress);
-		
+
 		practiceFormPage.testBasic.zoomOut();
-		
+
 		practiceFormPage.testBasic.inputText(practiceFormPage.mnState, inputState);
 		practiceFormPage.testBasic.keysEnter(practiceFormPage.mnState);
-		
+
 		testBasic.driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(2));
-		
+
 		practiceFormPage.testBasic.inputText(practiceFormPage.mnCity, inputCity);
 		practiceFormPage.testBasic.keysEnter(practiceFormPage.mnCity);
-		
+
 		practiceFormPage.testBasic.submitByLocator(practiceFormPage.btnSubmit);
 
 		PracticeFormResult practiceFormResult = new PracticeFormResult(testBasic.driver);
-		
+
 		String actualValueStudentName = practiceFormResult.getValueByLabel("Student Name");
 		String actualValueStudentEmail = practiceFormResult.getValueByLabel("Student Email");
 		String actualValueGender = practiceFormResult.getValueByLabel("Gender");
@@ -78,7 +65,7 @@ public class FormsTest extends TestCase {
 		String actualValuePicture = practiceFormResult.getValueByLabel("Picture");
 		String actualValueAddress = practiceFormResult.getValueByLabel("Address");
 		String actualValueStateAndCity = practiceFormResult.getValueByLabel("State and City");
-		
+
 		String expectedStudentName = inputFirstName + " " + inputLastName;
 		String expectedStudentEmail = inputEmail;
 		String expectedGender = inputGender;
@@ -89,7 +76,7 @@ public class FormsTest extends TestCase {
 		String expectedPicture = "Cong.jpg";
 		String expectedAddress = inputCurrentAddress;
 		String expectedStateAndCity = inputState + " " + inputCity;
-		
+
 //		Assert - TestNG : Compare actual vs expect
 		Assert.assertEquals(actualValueStudentName, expectedStudentName);
 		Assert.assertEquals(actualValueStudentEmail, expectedStudentEmail);
@@ -102,4 +89,49 @@ public class FormsTest extends TestCase {
 		Assert.assertEquals(actualValueAddress, expectedAddress);
 		Assert.assertEquals(actualValueStateAndCity, expectedStateAndCity);
 	}
+
+	@DataProvider(name = "FormData")
+	public String[][] formData() {
+		FormData formData = new FormData();
+		
+//		FormData formData = new FormData("Pham", "Cong", "abcd@gmail.com", "Male", "9", "December", "1998",
+//				"0355373838", "Maths", "English", "Ha Noi", "Sports", "Reading", "NCR", "Delhi");
+		
+		formData.inputFirstName = "Pham";
+		formData.inputLastName = "Cong";
+		formData.inputEmail = "abcd@gmail.com";
+		formData.inputGender = "Male";
+		formData.inputDay = "9";
+		formData.inputMonth = "December";
+		formData.inputYear = "1998";
+		formData.inputUserNumber = "0355373838";
+		formData.inputSubject1 = "Maths";
+		formData.inputSubject2 = "English";
+		formData.inputCurrentAddress = "Ha Noi";
+		formData.inputCheckbox1 = "Sports";
+		formData.inputCheckbox2 = "Reading";
+		formData.inputState = "NCR";
+		formData.inputCity = "Delhi";
+
+		String[][] listInput = new String[1][15];
+		listInput[0][0] = formData.inputFirstName;
+		listInput[0][1] = formData.inputLastName;
+		listInput[0][2] = formData.inputEmail;
+		listInput[0][3] = formData.inputGender;
+		listInput[0][4] = formData.inputDay;
+		listInput[0][5] = formData.inputMonth;
+		listInput[0][6] = formData.inputYear;
+		listInput[0][7] = formData.inputUserNumber;
+		listInput[0][8] = formData.inputSubject1;
+		listInput[0][9] = formData.inputSubject2;
+		listInput[0][10] = formData.inputCurrentAddress;
+		listInput[0][11] = formData.inputCheckbox1;
+		listInput[0][12] = formData.inputCheckbox2;
+		listInput[0][13] = formData.inputState;
+		listInput[0][14] = formData.inputCity;
+
+		return listInput;
+
+	}
+
 }
---
+++ DemoQA/src/main/java/tests/model/FormData.java
@@ -0,0 +1,47 @@
+package tests.model;
+
+public class FormData {
+	public String inputFirstName;
+	public String inputLastName;
+	public String inputEmail;
+	public String inputGender;
+	public String inputDay;
+	public String inputMonth;
+	public String inputYear;
+	public String inputUserNumber;
+	public String inputSubject1;
+	public String inputSubject2;
+	public String inputCurrentAddress;
+	public String inputCheckbox1;
+	public String inputCheckbox2;
+	public String inputState;
+	public String inputCity;
+	
+	public FormData(String inputFirstName, String inputLastName, String inputEmail, String inputGender, String inputDay,
+			String inputMonth, String inputYear, String inputUserNumber, String inputSubject1, String inputSubject2,
+			String inputCurrentAddress, String inputCheckbox1, String inputCheckbox2, String inputState,
+			String inputCity) {
+		this.inputFirstName = inputFirstName;
+		this.inputLastName = inputLastName;
+		this.inputEmail = inputEmail;
+		this.inputGender = inputGender;
+		this.inputDay = inputDay;
+		this.inputMonth = inputMonth;
+		this.inputYear = inputYear;
+		this.inputUserNumber = inputUserNumber;
+		this.inputSubject1 = inputSubject1;
+		this.inputSubject2 = inputSubject2;
+		this.inputCurrentAddress = inputCurrentAddress;
+		this.inputCheckbox1 = inputCheckbox1;
+		this.inputCheckbox2 = inputCheckbox2;
+		this.inputState = inputState;
+		this.inputCity = inputCity;
+	}
+	
+	public FormData() {
+		
+	}
+	
+	
+
+}
